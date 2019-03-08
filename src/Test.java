3e1db36bf9a28d770afd9e4d0cd02a2229fd347c
kpilszak <klaudia.pilszak@gmail.com>
2019-03-08 16:31:14 +0100

    Create Test.java

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+
+}
